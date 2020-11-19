f5fba938c41864486c1507511fc396f907969fcb
Tony Huang <t.tonyhuang@gmail.com>
2020-11-19 17:30:22 -0500

    deploy

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import './App.css';
 import axios from 'axios';
 
+
+
 function App() {
   const [comic, setComic] = useState();
   const [err, setErr] = useState();
@@ -9,7 +11,7 @@ function App() {
 
   useEffect(() => {
     setLoading(true);
-    const fetch = axios.get('https://cors-anywhere.herokuapp.com/http://xkcd.com/info.0.json');
+    const fetch = axios.get('/http://xkcd.com/info.0.json');
     fetch.then(response => {
       setComic(response.data)
       setLoading(false);
@@ -20,9 +22,9 @@ function App() {
     fetch.finally(() => setLoading(false));
   }, []);
 
-  const getComic = (num) => { 
+  const getComic = (number) => { 
     setLoading(true);
-    axios.get(`https://cors-anywhere.herokuapp.com/http://xkcd.com/${num}/info.0.json`)
+    axios.get(`/http://xkcd.com/${number}/info.0.json`)
       .then(response => setComic(response.data))
       .catch(err => {
         setErr(err);
@@ -39,7 +41,11 @@ function App() {
   return (
     <div className="App">
       {comic.title}
-      <button onClick={() => getComic(comic.num-1)}>
+      <button disabled={true} onClick={() => {
+        if (comic.num > 1){
+        getComic(comic.num-1)
+        }
+      }}>
         previous
       </button>
       <button onClick={() => getComic(comic.num+1)}>
