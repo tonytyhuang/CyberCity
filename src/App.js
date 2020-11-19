2c9e01343e2c25b361df1b7596d785fa9fc3e458
Tony Huang <t.tonyhuang@gmail.com>
2020-11-18 23:21:52 -0500

    embedded api, added next and previous buttons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-//import React, from 'react';
+import React, { useEffect, useState } from "react";
 import './App.css';
 import axios from 'axios';
 
 function App() {
+  const [comic, setComic] = useState();
+  const [err, setErr] = useState();
+  const [loading, setLoading] = useState();
+
+  useEffect(() => {
+    setLoading(true);
+    const fetch = axios.get('https://cors-anywhere.herokuapp.com/http://xkcd.com/info.0.json');
+    fetch.then(response => {
+      setComic(response.data)
+      setLoading(false);
+    });
+    fetch.catch(err => {
+      setErr(err);
+    })
+    fetch.finally(() => setLoading(false));
+  }, []);
+
+  const getComic = (num) => { 
+    setLoading(true);
+    axios.get(`https://cors-anywhere.herokuapp.com/http://xkcd.com/${num}/info.0.json`)
+      .then(response => setComic(response.data))
+      .catch(err => {
+        setErr(err);
+      })
+      .finally(() => setLoading(false));
+  };
+  if (loading) {
+    return <h1>Loading...</h1>
+  }
+  if(!comic || err){
+    return <h1>Error</h1>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {comic.title}
+      <button onClick={() => getComic(comic.num-1)}>
+        previous
+      </button>
+      <button onClick={() => getComic(comic.num+1)}>
+        next
+      </button>
+      <img src = {comic.img}/>
     </div>
   );
 }
