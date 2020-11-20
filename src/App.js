f1592306f0fe57fb896d23925150633ae98e3c1b
Tony Huang <t.tonyhuang@gmail.com>
2020-11-19 21:01:51 -0500

    add router and separate component

--- src/App.js
+++ src/App.js
@@ -1,85 +1,15 @@
-import React, { useEffect, useState } from "react";
 import './App.css';
-import axios from 'axios';
-
+import Comic from './Components/Comic.js';
+import {Switch, Route, Link} from 'react-router-dom';
 
 
 function App() {
-  const [comic, setComic] = useState();
-  const [err, setErr] = useState();
-  const [loading, setLoading] = useState();
-  const [current, setCurrent] = useState();
-
-  useEffect(() => {
-    setLoading(true);
-    const fetch = axios.get('https://cors-anywhere.herokuapp.com/http://xkcd.com/info.0.json');
-    fetch.then(response => {
-      setComic(response.data)
-      setLoading(false);
-    });
-    fetch.catch(err => {
-      setErr(err);
-    })
-    fetch.finally(() => setLoading(false));
-  }, []);
-
-  const getComic = (number) => { 
-    setLoading(true);
-    axios.get(`https://cors-anywhere.herokuapp.com/http://xkcd.com/${number}/info.0.json`)
-      .then(response => setComic(response.data))
-      .catch(err => {
-        setErr(err);
-      })
-      .finally(() => setLoading(false));
-  };
-
-  const getCurrComic = () => {
-    setLoading(true);
-    axios.get('https://cors-anywhere.herokuapp.com/http://xkcd.com/info.0.json')
-      .then(response => {
-        setComic(response.data);
-        setCurrent(response.data.num);
-        setLoading(false);
-      })
-      .catch(error => {
-        setErr(err);
-        setLoading(false);
-      });
-  }
-
-  const getRandom = () => {
-    const num = Math.floor(Math.random() * (current - 1)) + 1;
-    getComic(num);
-  }
-
-  useEffect(() => {
-    getCurrComic();
-  }, []);
-
-
-  if (loading) {
-    return <h1>Loading...</h1>
-  }
-  if(!comic || err){
-    return <h1>Error</h1>
-  }
-
   return (
-    <div className="App">
-      {comic.title}
-      <button disabled={comic.num <= 1} onClick={() => {
-        getComic(comic.num-1)
-      }}>
-        previous
-      </button>
-      <button disabled={comic.num == current} onClick={() =>{
-        getComic(comic.num+1)}}>
-        next
-      </button>
-      <button onClick={() => getRandom()} >
-        random
-      </button>
-      <img src = {comic.img}/>
+    <div className="app">
+      <Switch>
+        <Route path='/' component = {Comic} />
+        <Route path='/:comicNum' component = {Comic} />
+      </Switch>
     </div>
   );
 }
---
+++ src/Components/Comic.js
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+//import './Components/Comic.css';
+import axios from 'axios';
+
+const Comic = (props) => {
+    const comicNum = props.match.params.comicNum;
+    const [comic, setComic] = useState();
+    const [err, setErr] = useState();
+    const [loading, setLoading] = useState();
+    const [current, setCurrent] = useState();
+
+    useEffect(() => {
+        setLoading(true);
+        const fetch = axios.get('https://cors-anywhere.herokuapp.com/http://xkcd.com/info.0.json');
+        fetch.then(response => {
+            setComic(response.data)
+            setLoading(false);
+        });
+        fetch.catch(err => {
+            setErr(err);
+        })
+        fetch.finally(() => setLoading(false));
+    }, []);
+
+    const getComic = (number) => { 
+        setLoading(true);
+        axios.get(`https://cors-anywhere.herokuapp.com/http://xkcd.com/${number}/info.0.json`)
+        .then(response => setComic(response.data))
+        .catch(err => {
+            setErr(err);
+        })
+        .finally(() => setLoading(false));
+    };
+
+    const getCurrComic = () => {
+        setLoading(true);
+        axios.get('https://cors-anywhere.herokuapp.com/http://xkcd.com/info.0.json')
+        .then(response => {
+            setComic(response.data);
+            setCurrent(response.data.num);
+            setLoading(false);
+        })
+        .catch(error => {
+            setErr(err);
+            setLoading(false);
+        });
+    }
+
+    const getRandom = () => {
+        const num = Math.floor(Math.random() * (current - 1)) + 1;
+        getComic(num);
+    }
+
+    useEffect(() => {
+        getCurrComic();
+    }, []);
+
+    
+
+
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+    if(!comic || err){
+        return <h1>Error</h1>
+    }
+
+    return (
+    <div className="Comic">
+        {comic.title}
+        Date: {comic.month}/{comic.day}/{comic.year}
+        <button disabled={comic.num <= 1} onClick={() => {
+            getComic(comic.num-1)
+        }}>
+            previous
+        </button>
+        <button disabled={comic.num == current} onClick={() =>{
+            getComic(comic.num+1)}}>
+            next
+        </button>
+        <button onClick={() => getRandom()} >
+            random
+        </button>
+        <img src = {comic.img}/>
+    </div>
+  );
+}
+
+export default Comic;
